779b98008340e039eda7e090cb0da5633e7119a0
stirabos <stirabos@redhat.com>
2023-09-18 18:08:34 +0200

    Handle different OCP minors with multiple directories
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- generate-fbc.sh
+++ generate-fbc.sh
@@ -6,16 +6,16 @@ package_name="kubevirt-hyperconverged"
 
 helpFunction()
 {
-   echo "Usage: $0"
-   echo -e "\t--help :   see all commands of this script"
-   echo -e "\t--init-basic :   initialize a new composite fragment"
-   echo -e "\t--render : render the FBC fragments"
+   echo -e "Usage: $0\n"
+   echo -e "\t--help :   see all commands of this script\n"
+   echo -e "\t--init-basic :   initialize a new composite fragment\n\t  example: $0 --init-basic v4.13 yq\n"
+   echo -e "\t--render : render one FBC fragment\n\t  example: $0 --render v4.13\n"
    exit 1
 }
 
 devfile()
 {
-    cat <<EOT >> $1/devfile.yaml 
+    cat <<EOT > $1/devfile.yaml 
 schemaVersion: 2.2.0
 metadata:
   name: fbc-$1
@@ -28,8 +28,8 @@ components:
     image:
       imageName: ""
       dockerfile:
-        uri: catalog.Dockerfile
-        buildContext: "$1"
+        uri: $1/catalog.Dockerfile
+        buildContext: ""
   - name: kubernetes
     kubernetes:
       inlined: placeholder
@@ -46,29 +46,24 @@ commands:
 EOT
 }
 
-composite() {
-    cat <<EOT > composite.yaml 
-  - name: "$1"
-    destination:
-      path: $1/catalog
-    strategy:
-      name: basic
-      template:
-        schema: olm.builder.basic
-        config: 
-          input: components/$1.yaml
-          output: catalog.json
-EOT
-}
+dockerfile()
+{
+    cat <<EOT > $1/catalog.Dockerfile
+# The base image is expected to contain
+# /bin/opm (with a serve subcommand) and /bin/grpc_health_probe
+FROM registry.redhat.io/openshift4/ose-operator-registry:$1
 
-config() {
-    cat <<EOT >> catalog-config.yaml 
-  - name: "$1"
-    destination:
-      baseImage: quay.io/operator-framework/opm:v1.29
-      workingDir: .
-    builders:
-      - olm.builder.basic
+# Configure the entrypoint and command
+ENTRYPOINT ["/bin/opm"]
+CMD ["serve", "/configs", "--cache-dir=/tmp/cache"]
+
+# Copy declarative config root into image at /configs and pre-populate serve cache
+ADD catalog /configs
+RUN ["/bin/opm", "serve", "/configs", "--cache-dir=/tmp/cache", "--cache-only"]
+
+# Set DC-specific label for the location of the DC root directory
+# in the image
+LABEL operators.operatorframework.io.index.configs.v1=/configs
 EOT
 }
 
@@ -81,26 +76,27 @@ case $cmd in
         frag=$2
         from=registry.redhat.io/redhat/redhat-operator-index:$frag
         yqOrjq=$3
+        mkdir -p "${frag}/catalog/kubevirt-hyperconverged/" "${frag}/${frag}"
+	touch "${frag}/${frag}/.empty"
         case $yqOrjq in
             "yq")
-                touch components/$frag.yaml
-                opm render $from -o yaml | yq "select( .package == \"$package_name\" or .name == \"$package_name\")" | yq 'select(.schema == "olm.bundle") = {"schema": .schema, "image": .image}' | yq 'select(.schema == "olm.package") = {"schema": .schema, "name": .name, "defaultChannel": .defaultChannel}' >> components/$frag.yaml
+                touch ${frag}/graph.yaml
+                opm render $from -o yaml | yq "select( .package == \"$package_name\" or .name == \"$package_name\")" | yq 'select(.schema == "olm.bundle") = {"schema": .schema, "image": .image}' | yq 'select(.schema == "olm.package") = {"schema": .schema, "name": .name, "defaultChannel": .defaultChannel}' >> ${frag}/graph.yaml
             ;;
             "jq")
-                opm render $from | jq "select( .package == \"$package_name\" or .name == \"$package_name\")" | jq 'if (.schema == "olm.bundle") then {schema: .schema, image: .image} else (if (.schema == "olm.package") then {schema: .schema, name: .name, defaultChannel: .defaultChannel} else . end) end' >> components/$frag.json
+                opm render $from | jq "select( .package == \"$package_name\" or .name == \"$package_name\")" | jq 'if (.schema == "olm.bundle") then {schema: .schema, image: .image} else (if (.schema == "olm.package") then {schema: .schema, name: .name, defaultChannel: .defaultChannel} else . end) end' >> ${frag}/graph.json
             ;;
             *)
                 echo "please specify if yq or jq"
                 exit 1
             ;;
         esac
-        mkdir -p $frag "$frag/catalog"
-        devfile $frag 
-        composite $frag
-        config $frag
+        devfile $frag
+	dockerfile $frag
     ;;
     "--render")
-        opm alpha render-template composite -f catalog-config.yaml -c composite.yaml --validate
+	frag=$2
+        opm alpha render-template basic ${frag}/graph.yaml > ${frag}/catalog/kubevirt-hyperconverged/catalog.yaml
     ;;
     *)
         echo "$cmd not one of the allowed flags"
