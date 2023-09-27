142a9d8517ffc2505d5e04ba33114692d1666fbc
stirabos <stirabos@redhat.com>
2023-09-27 11:33:39 +0200

    Fix make sanity for local execution
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- generate-fbc.sh
+++ generate-fbc.sh
@@ -152,7 +152,8 @@ case $cmd in
     grep -E "^image: [brew\.]*registry.redhat.io/container-native-virtualization/hco-bundle-registry[-rhel9]*@sha256" "$frag"/graph.yaml | while read -r line ; do
       image=${line/image: /}
       echo "Processing $image"
-      url=$(${SKOPEO_CMD} inspect --no-tags "${AUTH_FILE}" docker://"$image" | grep "\"url\": ")
+      # shellcheck disable=SC2086
+      url=$(${SKOPEO_CMD} inspect --no-tags ${AUTH_FILE} docker://"$image" | grep "\"url\": ")
       tag1=${url/*\/images\/}
       tag=${tag1/\",/}
       sed -i "s|$image|$image\n# hco-bundle-registry $tag|g" "$frag"/graph.yaml
@@ -167,7 +168,8 @@ case $cmd in
       grep -E "^image: [brew\.]*registry.redhat.io/container-native-virtualization/hco-bundle-registry[-rhel9]*@sha256" "$frag"/graph.yaml | while read -r line ; do
         image=${line/image: /}
         echo "Processing $image"
-        url=$(${SKOPEO_CMD} inspect --no-tags "${AUTH_FILE}" docker://"$image" | grep "\"url\": ")
+	# shellcheck disable=SC2086
+        url=$(${SKOPEO_CMD} inspect --no-tags ${AUTH_FILE} docker://"$image" | grep "\"url\": ")
         tag1=${url/*\/images\/}
         tag=${tag1/\",/}
         sed -i "s|$image|$image\n# hco-bundle-registry $tag|g" "$frag"/graph.yaml
